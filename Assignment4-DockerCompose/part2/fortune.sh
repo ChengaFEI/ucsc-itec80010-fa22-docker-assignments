69a30db85e5bc7846d947e3a81ac025bd4516cdb
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-02-26 15:06:28 -0500

    complete hw4 part2 docker compose

--- Assignment4-DockerCompose/part2/fortune.sh
+++ Assignment4-DockerCompose/part2/fortune.sh
@@ -10,7 +10,7 @@ echo "Configured to write fortune to $OUTPUT_FILE every $INTERVAL seconds"
 while :
 do
   echo "$(date) Writing fortune..."
-  fortune > $OUTPUT_FILE
+  echo "fortune" >> $OUTPUT_FILE
   sleep $INTERVAL
 done
 
